c5563a09dd03d5a3f0f59a80a4179fb8abe1973b
Mayurs0 <mayurs@iconnectsolutions.com>
2025-05-31 15:12:58 +0530

    [REFACT]: user registr flow completed database schema added password decryption added

--- actions/register.ts
+++ actions/register.ts
@@ -2,15 +2,34 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
-  console.log("validatedFields", validatedFields);
-
   if (!validatedFields.success) {
     return { error: "Invalid fields" };
   }
+  const { name, email, password } = validatedFields.data;
+  const hashPassword = await bcrypt.hash(password, 10);
 
-  return { sucess: "Register email sent" };
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already taken!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashPassword,
+    },
+  });
+
+  //TODO : send verification token in response
+
+  return { sucess: "User created" };
 };
